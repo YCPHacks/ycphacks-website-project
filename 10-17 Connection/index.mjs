62752a7d2759506e840c9377cae9a24347c4b725
dile0 <dile@ycp.edu>
2022-10-13 19:07:56 -0400

    Trying to fix my GitHub (might delete this branch)
    Having issues with GitHub and solutions say I need to merge my changes

--- 10-17 Connection/index.mjs
+++ 10-17 Connection/index.mjs
@@ -1,21 +1,46 @@
+import 'dotenv/config';
+
 import express from 'express';
+import mysqlx from '@mysql/xdevapi';
+
+import config from './config.mjs';
 
 const app = express();
+
+app.set('view engine', 'pug');
 
 const port = process.env.PORT || 7777;
 
 app.get('/users/:user_id', async (req, res) => {
-  const request = await fetch(`http://localhost:7777/api/users/${req.params.user_id}`);
+  const request = await fetch(`http://localhost:${port}/api/users/${req.params.user_id}`);
   const response = await request.json();
 
-  res.status(200).send(response.id);
+  // Render in a Pug template.
+  res.status(200).render('index', response);
 });
 
-app.get('/api/users/:user_id', (req, res) => {
-  res.status(200).send({ id: req.params.user_id });
-  // res.status(200).json({ id: req.params.user_id });
+app.get('/api/users/:user_id', async (req, res) => {
+  const session = await mysqlx.getSession(config);
+
+  const table = session
+      .getSchema('prototype_schema')
+      .getTable('prototype_table');
+
+  const result = await table.select().execute();
+
+  // Gets the columns for the table.
+  const columns = result.getColumns();
+
+  // Creates an array of column names.
+  const columnNames = [ columns[0].getColumnName(),
+    columns[1].getColumnName() ];
+
+  // Send an object containing columns and rows from table.
+  res.status(200).send({ columns: columnNames, rows: result.fetchAll() });
+
+  session.close();
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
-})
+});
