739ae057083a51c2a86938adb8c48cac1f584f83
Kayla Hunt <khunt6@ycp.edu>
2023-03-30 00:39:56 -0400

    dashboard updates
    file restraints

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,4 +1,4 @@
-async function verify(){
+async function verify() {
     'use strict'
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
@@ -15,10 +15,9 @@ async function verify(){
         }, false)
     })
 
-    const resume = document.getElementById('resume');
-
 
 }
+
 function show() {
     const form = document.getElementById('allergyInput');
     const submit = document.getElementById('tutorial');
@@ -26,10 +25,24 @@ function show() {
     // submit.style.visibility = 'visible';
     submit.style.display = 'block';
 }
+
 function hide() {
     const form = document.getElementById('allergyInput');
     const submit = document.getElementById('tutorial');
     form.removeAttribute("required", "");
     form.value = '';
     submit.style.display = 'none';
+}
+
+function validateSize(input) {
+    const fileSize = input.files[0].size / 1024 / 1024; // in MiB
+    console.log(input.files[0].name);
+
+    if (fileSize > 2) {
+        alert('File size exceeds 2 MiB, please try uploading something smaller!');
+        input.value = null;
+        console.log("successfully reset");
+    } else {
+        // alert("its fine");
+    }
 }
